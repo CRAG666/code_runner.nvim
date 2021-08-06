f9696b5828a36777a6f45375e9c5d4dcb256f591
Crag <ogeidca49@gmail.com>
2021-08-05 22:31:49 -0500

    change path config file

--- lua/code_runner.lua
+++ lua/code_runner.lua
@@ -20,7 +20,7 @@ M.run_code = function()
 end
 
 M.open_filetype_suported = function()
-  command ="tabnew " .. "code_runner/code_runner.json"
+  command ="tabnew " ..fn.stdpath('data') .. '/site/pack/packer/start/code_runner.nvim/lua/code_runner/code_runner.json'
   vim.cmd(command)
 end
 
