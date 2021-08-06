7e457c376721a3a41e628f1dd5c44a7c4ff49347
Crag <ogeidca49@gmail.com>
2021-08-06 12:24:55 -0500

    funtional version

--- lua/code_runner/commands.lua
+++ lua/code_runner/commands.lua
@@ -1,9 +1,18 @@
 local o = require("code_runner.options")
+-- load json config and convert to table
 local loadTable = require("code_runner.load_json")
 local fileCommands = loadTable()
+
+-- if json file not exists return null
+if not fileCommands then
+	return
+end
+
+-- Create prefix for run commands
 local prefix = string.format("%s %dsplit term://", o.get().term.position, o.get().term.size)
 local suffix = "<CR>"
 
+-- Create autocmd for each file type and add map for ececute file type
 local function shellcmd(lang, command)
 	vim.cmd ("autocmd FileType " .. lang .. " nnoremap <buffer> " .. o.get().map .. " :" .. prefix .. "" .. command .. suffix)
 end
@@ -12,13 +21,25 @@ local function vimcmd(lang, config)
 	vim.cmd ("autocmd FileType " .. lang .. " nnoremap <buffer> " .. o.get().map .. " :" .. config .. "<CR>")
 end
 
+-- Substitute json vars to vim vars in commands for each file type.
+-- If a command has no arguments, one is added with the current file path
 local function subvarcomm(command)
-	local vars_json = {["$file"] = "%", ["$fileName"] = "%:t", ["$fileNameWithoutExt"] = "%:r", ["$dir"] = "%:p:h"}
+	local vars_json = {
+		["%$fileNameWithoutExt"] = "%%:r",
+		["$fileName"] = "%%:t",
+		["$file"] = "%%",
+		["$dir"] = "%%:p:h"
+	}
 	for var, var_vim in pairs(vars_json) do
 		command = command:gsub(var, var_vim)
 	end
+	if not command:find("%%") then
+		command = command .. " %"
+	end
+	return command
 end
 
+-- call subvarcomm and shellcmd
 function run()
 	for lang, command in pairs(fileCommands) do
 		command_vim = subvarcomm(command)
