0cefd759a0ba611aecf32d9aa379f5943899cbbb
brian.orwe <brian.orwe@gmail.com>
2021-08-08 12:47:54 +0300

    Fixed searching for data path instead of HOME, windows, doesn't have home

--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -4,7 +4,7 @@ local options = {
         size = 8
       },
       map = "<leader>r",
-      json_path = os.getenv("HOME") .. '/.local/share/nvim/site/pack/packer/start/code_runner.nvim/lua/code_runner/code_runner.json'
+      json_path = vim.fn.stdpath("data") .. '/site/pack/packer/start/code_runner.nvim/lua/code_runner/code_runner.json'
 }
 
 local M = {}
