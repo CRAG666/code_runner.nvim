c94c7ee35fe3021efb322513d5c02d950ce3e803
Crag <ogeidca49@gmail.com>
2021-10-28 11:31:06 -0500

    better config and delete maps

--- lua/code_runner.lua
+++ lua/code_runner.lua
@@ -3,41 +3,36 @@ local og = options.get()
 local commands = require("code_runner.commands")
 local M = {}
 
--- Load json config and convert to table
-local loadTable = require("code_runner.load_json")
 
 M.setup = function(user_options)
   options.set(user_options)
-  vim.cmd([[lua require('code_runner').load_json_files()]])
+  M.load_json_files()
   vim.api.nvim_exec(
     [[
-  function! CRunnerGetKeysForCmds(Arg,Cmd,Curs)
-    let cmd_keys = ""
-    for x in keys(g:fileCommands)
-      let cmd_keys = cmd_keys.x."\n"
-    endfor
-    return cmd_keys
-  endfunction
+      function! CRunnerGetKeysForCmds(Arg,Cmd,Curs)
+        let cmd_keys = ""
+        for x in keys(g:fileCommands)
+          let cmd_keys = cmd_keys.x."\n"
+        endfor
+        return cmd_keys
+      endfunction
 
-  command! CRFiletype lua require('code_runner').open_filetype_suported()
-  command! CRProjects lua require('code_runner').open_project_manager()
-  command! -nargs=? -complete=custom,CRunnerGetKeysForCmds RunCode lua require('code_runner').run_code("<args>")
-  command! RunFile lua require('code_runner').run_filetype()
-  command! RunProject lua require('code_runner').run_project()
-  ]],
+      command! CRFiletype lua require('code_runner').open_filetype_suported()
+      command! CRProjects lua require('code_runner').open_project_manager()
+      command! -nargs=? -complete=custom,CRunnerGetKeysForCmds RunCode lua require('code_runner').run_code("<args>")
+      command! RunFile lua require('code_runner').run_filetype()
+      command! RunProject lua require('code_runner').run_project()
+    ]],
     false
   )
-  if og.filetype.map == og.project_context.map then
-    vim.api.nvim_set_keymap("n", og.filetype.map, ":RunCode<CR>", { noremap = true })
-  else
-    vim.api.nvim_set_keymap("n", og.filetype.map, ":RunFile<CR>", { noremap = true })
-    vim.api.nvim_set_keymap("n", og.project_context.map, ":RunProject<CR>", { noremap = true })
-  end
 end
 
+
 M.load_json_files = function()
-  vim.g.fileCommands = loadTable(og.filetype.json_path)
-  vim.g.projectManager = loadTable(og.project_context.json_path)
+  -- Load json config and convert to table
+  local load_json_as_table = require("code_runner.load_json")
+  vim.g.fileCommands = load_json_as_table(og.filetype_path)
+  vim.g.projectManager = load_json_as_table(og.project_path)
 
   -- Message if json file not exist
   if not vim.g.fileCommands then
@@ -67,11 +62,11 @@ local function open_json(json_path)
 end
 
 M.open_filetype_suported = function()
-  open_json(og.filetype.json_path)
+  open_json(og.filetype_path)
 end
 
 M.open_project_manager = function()
-  open_json(og.project_context.json_path)
+  open_json(og.project_path)
 end
 
 return M
--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -3,23 +3,14 @@ local options = {
     position = "belowright",
     size = 8,
   },
-  filetype = {
-    map = "<leader>r",
-    json_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/code_runner.nvim/lua/code_runner/code_runner.json",
-  },
-  project_context = {
-    map = "<leader>r",
-    json_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/code_runner.nvim/lua/code_runner/project_manager.json",
-  },
+  filetype_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/code_runner.nvim/lua/code_runner/code_runner.json",
+  project_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/code_runner.nvim/lua/code_runner/project_manager.json",
 }
 
 local M = {}
 -- set user config
 M.set = function(user_options)
-  for key, value in pairs(options) do
-    local u_o = user_options[key] or {}
-    options[key] = vim.tbl_extend("force", value, u_o)
-  end
+  options = vim.tbl_deep_extend("force", options, user_options)
 end
 
 -- get user options
