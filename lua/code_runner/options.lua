44e70390cba0073bf6dd41e927a12f36d411989f
Crag <ogeidca49@gmail.com>
2022-02-11 11:03:36 -0600

    inner silla

--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -13,9 +13,8 @@ local options = {
 local M = {}
 -- set user config
 M.set = function(user_options)
-	if options.mode then
-		options.mode = "&& :" .. options.mode
-		print(vim.inspect("Entre"))
+	if user_options.mode then
+		user_options.mode = "&& :" .. user_options.mode
 	end
   options = vim.tbl_deep_extend("force", options, user_options)
 end
