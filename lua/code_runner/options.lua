a4ff50c2445ae8348d046cfed48982e1ae0880b8
Crag <ogeidca49@gmail.com>
2022-02-12 15:38:30 -0600

    character error

--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -20,7 +20,7 @@ M.set = function(user_options)
 	end
 	options = vim.tbl_deep_extend("force", options, user_options)
 	if options.term.tab then
-		options.prefix = ":terminal "
+		options.prefix = "| :terminal "
 	else
 		options.prefix = string.format("%s %dsplit term://", options.term.position, options.term.size)
 	end
