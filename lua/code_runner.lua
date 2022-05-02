2d51a230bbc67d486d239339ac99f77d258ea628
Crag <ogeidca49@gmail.com>
2022-05-02 16:25:19 -0500

    Remove vimscript dependencies

--- lua/code_runner.lua
+++ lua/code_runner.lua
@@ -2,55 +2,27 @@ local commands = require("code_runner.commands")
 local o = require("code_runner.options")
 local M = {}
 
-M.setup = function(user_options)
-  o.set(user_options or {})
-  M.load_runners()
-  vim.api.nvim_exec(
-    [[
-      function! RunKeyCompletion(lead, cmd, cursor)
-        echo a:cmd
-        echo a:cursor
-        let l = len(a:lead) - 1
-        if l >= 0
-          let filtered_args = copy(g:crSupportedFileTypes)
-          call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
-          if !empty(filtered_args)
-            return filtered_args
-          endif
-        endif
-        return g:crSupportedFileTypes
-      endfunction
-
-      function! RunnerCompletion(lead, cmd, cursor)
-        let valid_args = ['float', 'tab', 'term', 'toggle', 'toggleterm']
-        let l = len(a:lead) - 1
-        if l >= 0
-          let filtered_args = copy(valid_args)
-          call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
-          if !empty(filtered_args)
-            return filtered_args
-          endif
-        endif
-        return valid_args
-      endfunction
-
-			command! CRProjects lua require('code_runner').open_project_manager()
-      command! CRFiletype lua require('code_runner').open_filetype_suported()
-			command! -nargs=? -complete=customlist,RunKeyCompletion RunCode lua require('code_runner').run_code("<args>")
-      command! -nargs=? -complete=customlist,RunnerCompletion RunFile lua require('code_runner').run_filetype("<args>")
-      command! -nargs=? -complete=customlist,RunnerCompletion RunProject lua require('code_runner').run_project("<args>")
-			command! RunClose lua require('code_runner').run_close()
-		]] ,
-    false
-  )
-end
+M.run_code = commands.run
+M.run_filetype = commands.run_filetype
+M.run_project = commands.run_project
+M.run_close = commands.run_close
+M.get_filetype_command = commands.get_filetype_command
+M.get_project_command = commands.get_project_command
 
 local function open_json(json_path)
   local command = "tabnew " .. json_path
   vim.cmd(command)
 end
 
-M.load_runners = function()
+M.open_filetype_suported = function()
+  open_json(o.get().filetype_path)
+end
+
+M.open_project_manager = function()
+  open_json(o.get().project_path)
+end
+
+local function load_runners()
   -- Load json config and convert to table
   local opt = o.get()
   local load_json_as_table = require("code_runner.load_json")
@@ -68,28 +40,52 @@ M.load_runners = function()
   -- Message if json file not exist
   if vim.tbl_isempty(opt.filetype) then
     vim.notify(
-      "Not exist command for filetypes or format invalid, if use json please execute :CRFiletype or if use lua edit setup",
-      vim.log.levels.ERROR,
-      { title = "Code Runner Error" }
+    "Not exist command for filetypes or format invalid, if use json please execute :CRFiletype or if use lua edit setup",
+    vim.log.levels.ERROR,
+    { title = "Code Runner Error" }
     )
-  else
-    vim.g.crSupportedFileTypes = vim.tbl_keys(opt.filetype)
   end
 end
 
-M.run_code = commands.run
-M.run_filetype = commands.run_filetype
-M.run_project = commands.run_project
-M.run_close = commands.run_close
-M.get_filetype_command = commands.get_filetype_command
-M.get_project_command = commands.get_project_command
-
-M.open_filetype_suported = function()
-  open_json(o.get().filetype_path)
+local function completion(ArgLead, options)
+  local filterd_args = vim.tbl_filter(function(v) return v:find(ArgLead:lower(), 1, true) == 1 end, options)
+  if not vim.tbl_isempty(filterd_args) then
+    return filterd_args
+  end
+  return options
 end
 
-M.open_project_manager = function()
-  open_json(o.get().project_path)
+M.setup = function(user_options)
+  o.set(user_options or {})
+  load_runners()
+  local simple_cmds = {
+    RunClose = commands.run_close,
+    CRFiletype = M.open_filetype_suported,
+    CRProjects = M.open_project_manager
+  }
+  for cmd,func in pairs(simple_cmds) do
+    vim.api.nvim_create_user_command(cmd, func, {nargs=0})
+  end
+  local valid_filetypes = vim.tbl_keys(o.get().filetype)
+  vim.api.nvim_create_user_command('RunCode', commands.run, {
+    nargs = '?',
+    complete = function(ArgLead, CmdLine, CursorPos)
+      return completion(ArgLead, valid_filetypes)
+    end,
+  })
+  local modes = {'float', 'tab', 'term', 'toggle', 'toggleterm'}
+  vim.api.nvim_create_user_command('RunFile', function(opts) commands.run_filetype(opts.args) end, {
+    nargs = '?',
+    complete = function(ArgLead, CmdLine, CursorPos)
+      return completion(ArgLead, modes)
+    end,
+  })
+  vim.api.nvim_create_user_command('RunProject',function(opts) commands.run_project(opts.args) end, {
+    nargs = '?',
+    complete = function(ArgLead, CmdLine, CursorPos)
+      return completion(ArgLead, modes)
+    end,
+  })
 end
 
 return M
