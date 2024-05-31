843b3476cec91a53d1168a6f4089134958ecd07e
rokbot <29069815+rokbot@users.noreply.github.com>
2024-05-31 17:46:27 -0400

    refactor!: Update to neovim >= 0.10
    BREAKING CHANGE: Remove neovim deprecated APIs.

--- lua/code_runner/floats.lua
+++ lua/code_runner/floats.lua
@@ -4,14 +4,19 @@ local o = require("code_runner.options")
 function M.floating(command)
   local opt = o.get()
   local buf = vim.api.nvim_create_buf(false, true)
+
   vim.api.nvim_buf_set_keymap(buf, "t", "<ESC>", "<C-\\><C-n>", { silent = true })
   vim.api.nvim_buf_set_keymap(buf, "n", opt.float.close_key, "<CMD>q!<CR>", { silent = true })
-  vim.api.nvim_buf_set_option(buf, "filetype", "crunner")
-  local win_height = math.ceil(vim.api.nvim_get_option("lines") * opt.float.height - 4)
-  local win_width = math.ceil(vim.api.nvim_get_option("columns") * opt.float.width)
-  local row = math.ceil((vim.api.nvim_get_option("lines") - win_height) * opt.float.y - 1)
-  local col = math.ceil((vim.api.nvim_get_option("columns") - win_width) * opt.float.x)
-  local opts = {
+  vim.api.nvim_set_option_value("filetype", "crunner", { buf = buf })
+
+  -- stylua: ignore start
+  local win_height = math.ceil(vim.api.nvim_get_option_value("lines", { filetype = "crunner" }) * opt.float.height - 4)
+  local win_width = math.ceil(vim.api.nvim_get_option_value("columns", { filetype = "crunner" }) * opt.float.width)
+  local row = math.ceil((vim.api.nvim_get_option_value("lines", { filetype = "crunner" }) - win_height) * opt.float.y - 1)
+  local col = math.ceil((vim.api.nvim_get_option_value("columns", { filetype = "crunner" }) - win_width) * opt.float.x)
+  -- stylua: ignore end
+
+  local config = {
     style = "minimal",
     relative = "editor",
     border = opt.float.border,
@@ -20,16 +25,19 @@ function M.floating(command)
     row = row,
     col = col,
   }
-  local win = vim.api.nvim_open_win(buf, true, opts)
+  local win_id = vim.api.nvim_open_win(buf, true, config)
+
+  vim.api.nvim_set_option_value(
+    "winhl",
+    "Normal:" .. opt.float.float_hl .. ",FloatBorder:" .. opt.float.border_hl,
+    { win = win_id }
+  )
+  vim.api.nvim_set_option_value("winblend", opt.float.blend, { win = win_id })
+
   vim.fn.termopen(command)
   if opt.startinsert then
     vim.cmd("startinsert")
   end
-  if opt.wincmd then
-    vim.cmd("wincmd p")
-  end
-  vim.api.nvim_win_set_option(win, "winhl", "Normal:" .. opt.float.float_hl .. ",FloatBorder:" .. opt.float.border_hl)
-  vim.api.nvim_win_set_option(win, "winblend", opt.float.blend)
 end
 
 return M
