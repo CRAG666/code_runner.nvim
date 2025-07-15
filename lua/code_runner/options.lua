3038152f18ee7be1a338353c5f15fcfc48b58388
Ro <29069815+rokbot@users.noreply.github.com>
2025-07-15 04:13:41 -0400

    feat: :label: Update types

--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -1,20 +1,20 @@
 ---@type CodeRunnerOptions
-local options = {
+local default_options = {
   mode = "term", -- default display mode
   hot_reload = true, -- hot reload enabled/disabled
   focus = false, -- Focus window/viewport on the buffer (only works term and tab display mode)
   startinsert = false, -- startinsert (see ':h inserting-ex')
   insert_prefix = "", --  this key is controlled by startinsert
-  term = {
-    position = "vert", --  Position to open the terminal, this option is ignored if mode ~= term
-    size = 64, -- window size, this option is ignored if mode == tab
+  term = { -- this configs are ignored if mode ~= "term"
+    position = "vert", --  Position to open the terminal,
+    size = 64, -- window size, if position 'bot'(bottom) 8 is a good default
   },
   float = {
     close_key = "<ESC>",
     border = "solid", -- Window border (see ':h nvim_open_win()' ; /border)
 
-    height = 0.8, -- number from `0 - 1` for measurements
-    width = 0.8, -- number from `0 - 1` for measurements
+    height = 0.8,
+    width = 0.8,
     x = 0.5,
     y = 0.5,
 
@@ -23,9 +23,9 @@ local options = {
 
     blend = 0, -- Transparency (see ':h winblend')
   },
-  better_term = {  -- Toggle mode replacement
+  better_term = { -- Toggle mode replacement
     clean = false, -- Clean terminal before launch
-    number = 10,   -- Use nil for dynamic number and set init
+    number = 10, -- Use nil for dynamic number and set init
     init = nil,
   },
   filetype_path = "",
@@ -75,18 +75,18 @@ local function concat(v)
 end
 
 -- set user config
----@param user_options table
+---@param user_options CodeRunnerUserOptions
 M.set = function(user_options)
   if user_options.startinsert then
     user_options.insert_prefix = "startinsert"
   end
-  options = vim.tbl_deep_extend("force", options, user_options)
-  options.filetype = vim.tbl_map(concat, options.filetype)
-  options.prefix = string.format("%s %d new", options.term.position, options.term.size)
+  default_options = vim.tbl_deep_extend("force", default_options, user_options)
+  default_options.filetype = vim.tbl_map(concat, default_options.filetype)
+  default_options.prefix = string.format("%s %d new", default_options.term.position, default_options.term.size)
 end
 
 M.get = function()
-  return options
+  return default_options
 end
 
 return M
--- types/options.lua
+++ types/options.lua
@@ -3,7 +3,7 @@
 --- Don't edit or require this file
 error("Requiring a meta file")
 
--- Definition Options
+-- Code Runner Options (Library)
 ---@class CodeRunnerOptions
 ---@field mode CodeRunnerDisplayMode
 ---@field hot_reload boolean
@@ -11,23 +11,10 @@ error("Requiring a meta file")
 ---@field startinsert boolean
 ---@field filetype table
 ---@field prefix string
----@field term table
+---@field term CodeRunnerOptionsTermTable
 ---@field float CodeRunnerOptionsFloatTable
 
--- TODO: WIP improve typing
----@class CodeRunnerOptionsFloatTable
----@field close_key string keymap to close (default: <ESC>)
----@field height number
----@field width number
----@field x number
----@field y number
----@field border ("none"|"single"|"double"|"rounded"|"solid"|"shadow") | string[]  string[]: length must be 8 e.g {"╔", "═" ,"╗", "║", "╝", "═", "╚", "║"}
----@field blend number
----@field border_hl string
----@field float_hl string
-
 ---@type CodeRunnerDisplayMode
-
 ---@alias CodeRunnerDisplayMode
 ---|'"term"'
 ---|'"float"'
@@ -35,17 +22,41 @@ error("Requiring a meta file")
 ---|'"better_term"'
 ---|'"toggleterm"'
 
--- User Options
+--  configuration for term display mode
+---@class CodeRunnerOptionsTermTable
+---@field position ("vert"|"bot")
+---@field size number
+
+---@class CodeRunnerOptionsFloatTable
+---@field close_key string keymap to close (default: <ESC>)
+---@field height number number from `0 - 1` e.g 0.8 for measurements
+---@field width number number from `0 - 1` e.g 0.8 for measurements
+---@field x number
+---@field y number
+---@field border ("none"|"single"|"double"|"rounded"|"solid"|"shadow") | string[]  string[]: length must be 8 e.g {"╔", "═" ,"╗", "║", "╝", "═", "╚", "║"}
+---@field blend number
+---@field border_hl string
+---@field float_hl string
+
+
+-- User Options (Client)
 ---@class CodeRunnerUserOptions : CodeRunnerOptions
 ---@field mode? CodeRunnerDisplayMode
 ---@field hot_reload? boolean enabled/disabled (only works with 'term' or term like display modes)
 ---@field focus? boolean Focus on runner window/viewport (only works on 'term' and 'tab' display mode)
+---@field insert_prefix? string
 ---@field startinsert? boolean startinsert (see ':h inserting-ex') if focus is false has no effect unless display mode is 'float'
----@field filetype? table
+---@field filetype? table ft entries, is unknown run :=vim.bo.ft
 ---@field prefix? string
----@field term? table
+---@field term? CodeRunnerUserOptionsTermTable
 ---@field float? CodeRunnerUserOptionsFloatTable
 
+--  configuration for term display mode
+---@class CodeRunnerUserOptionsTermTable
+---@field position? ("vert"|"bot")
+---@field size? number
+
+--  configuration for float display mode
 ---@class CodeRunnerUserOptionsFloatTable
 ---@field close_key? string keymap to close (default: <ESC>)
 ---@field height? number
